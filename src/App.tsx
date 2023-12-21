73c0e6d030d74675c9b701e20c3422b1673eb1bf
Matt Gallucci <mattgallucci97@gmail.com>
2023-12-20 22:08:00 -0800

    added theme, linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // pages
-import { 
-  SplashPage, 
-  LoginPage, 
-  RegisterPage, 
-  ProfilePage, 
-  Dashboard, 
+import {
+  SplashPage,
+  LoginPage,
+  RegisterPage,
+  ProfilePage,
+  Dashboard,
   ClientPage,
-  ResetPasswordPage 
+  ResetPasswordPage,
 } from './components/pages';
 // components
 import ProtectedRoute from './components/ProtectedRoute';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme';
 
 function App() {
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/home' element={<SplashPage />} />
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/resetpassword/:token/:username' element={<ResetPasswordPage />} />
-          {/* All routes below are priveleged and need to be wrapped in a PrivateRoutes component */}
-          <Route element={<ProtectedRoute />}>
-            <Route path='/profile' element={<ProfilePage />} />
-            <Route path='/clients' element={<Dashboard />} />
-            <Route path='/client/:id' element={<ClientPage />} />
-            <Route path='/client-archive' element={<Dashboard archiveMode />} />
-          </Route>
-        </Routes>
-      </Router>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/home" element={<SplashPage />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/resetpassword/:token/:username" element={<ResetPasswordPage />} />
+            {/* All routes below are priveleged and need to be wrapped in a PrivateRoutes component */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/clients" element={<Dashboard />} />
+              <Route path="/client/:id" element={<ClientPage />} />
+              <Route path="/client-archive" element={<Dashboard archiveMode />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ChakraProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
