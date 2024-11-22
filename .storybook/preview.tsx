f8475963eddda2f66e50b8ba511e8dd0b094ec12
Matt Gallucci <mattgallucci97@gmail.com>
2024-11-22 13:30:03 -0800

    storybook setup

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,18 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
+import { AppProvider } from "../src/app/provider";
+import "../src/index.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => {
+      return (
+        <AppProvider>
+          <Story />
+        </AppProvider>
+      );
+    },
+  ],
   parameters: {
     controls: {
       matchers: {
